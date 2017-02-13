76a7ee5bb35c5cd9d068589fb9c78bb36265187c
Nicolas A. Barriga <nbarriga@gmail.com>
2017-02-13 13:17:40 -0700

    New all convolutional network.

--- runTraining.sh
+++ runTraining.sh
@@ -9,8 +9,14 @@ cp *.prot* old/$NOW/$1/
 # python ../../python/draw_net.py  train_rts.prototxt old/$NOW-$1/rts.png
 
 echo "Starting to train RTSnet ..."
-#~/git-working/caffe/build/tools/caffe train --gpu all --solver rts_solver.prototxt 2>&1 | tee logs/log$1.log old/$NOW/$1/log.log | grep --color -E 'Test\ .*$|$'
-~/git-working/caffe/build/tools/caffe train --gpu 0 --solver rts_solver.prototxt --weights snapshots/24-12-67200_iter_320000.caffemodel 2>&1 | tee logs/log$1.log old/$NOW/$1/log.log | grep --color -E 'Test\ .*$|$'
+#Start from scratch
+#~/git-working/caffe/build/tools/caffe train --gpu 0 --solver rts_solver.prototxt 2>&1 | tee logs/log$1.log old/$NOW/$1/log.log | grep --color -E 'Test\ .*$|$'
+
+#Start from pre-trained weights
+#~/git-working/caffe/build/tools/caffe train --gpu 0 --solver rts_solver.prototxt --weights snapshots/24x24new-drop.2_iter_400000.caffemodel 2>&1 | tee logs/log$1.log old/$NOW/$1/log.log | grep --color -E 'Test\ .*$|$'
+
+#Continue training (loads weights and solver state (LR, etc...))
+#~/git-working/caffe/build/tools/caffe train --gpu 0 --solver rts_solver.prototxt --snapshot snapshots/24x24new_iter_50000.solverstate 2>&1 | tee logs/log$1.log old/$NOW/$1/log.log | grep --color -E 'Test\ .*$|$'
 
 # 2> log1.log
 
