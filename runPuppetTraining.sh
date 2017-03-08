7624d986304c78fe72367bb9ee2a1ae7ebd407fa
Nicolas A. Barriga <nbarriga@gmail.com>
2017-03-08 15:08:11 -0700

    New python data layer to load compressed data into caffe. Needed for actionvector.

--- runPuppetTraining.sh
+++ runPuppetTraining.sh
@@ -14,13 +14,13 @@ echo "Starting to train RTSnet ..."
 #ln -s /common/barriga/puppet-data/train128x128_list.txt train_list.txt
 #ln -s /common/barriga/puppet-data/test128x128_list.txt test_list.txt
 sed "\
-	s/net:.*/net: \"puppet.prototxt\"/g;\
+	s/net:.*/net: \"actionvector.prototxt\"/g;\
 	s/base_lr:.*/base_lr: 1e-4/g;\
-	s/test_iter:.*/test_iter: 4/g;\
+	s/test_iter:.*/test_iter: 5/g;\
 	s/test_interval:.*/test_interval: 100/g;\
 	s/display:.*/display: 20/g;\
-	s/max_iter:.*/max_iter: 100000/g;\
+	s/max_iter:.*/max_iter: 200000/g;\
 	s/snapshot:.*/snapshot: 5000/g;\
-	s/snapshot_prefix:.*/snapshot_prefix: \"snapshots\/puppet128x128\"/g;\
+	s/snapshot_prefix:.*/snapshot_prefix: \"snapshots\/actionvector128\"/g;\
 	" rts_solver.prototxt >rts_solver.prototxt.tmp
-~/git-working/caffe/build/tools/caffe train --gpu 0 --solver rts_solver.prototxt.tmp 2>&1 | tee logs/logPuppet128x128.log | grep --color -E 'Test\ .*$|$'
+~/git-working/caffe/build/tools/caffe train --gpu 1 --solver rts_solver.prototxt.tmp 2>&1 | tee logs/actionvector128.log | grep --color -E 'Test\ .*$|$'
